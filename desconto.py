7bc553abdaedd2fea4e2037d5bffc9e28849948a
Kevin Garcia <kevingarciarp@gmail.com>
2024-02-08 21:27:35 -0300

    Calcular descontos

--- desconto.py
+++ desconto.py
@@ -1,11 +1,9 @@
-print('Bem-vindo a loja do Kevin G. Porto') #Cabeçalho!
+print('Bem-vindo a loja do Kevin G. Porto')
 
-# entrada para inserir o valor do produto e quantidade
 valorProduto = float(input('Digite o valor desejado:'))
 quantidadeProduto = int(input('Digite a quantidade desejada:'))
 descontoProduto = 0
 
-#desconto c baseado na quantidade
 if quantidadeProduto < 200:
     descontoProduto = 0.00
 elif 200 <= quantidadeProduto < 1000:
@@ -15,10 +13,8 @@ elif 1000 <= quantidadeProduto < 2000:
 else:
     descontoProduto = 0.15
 
-# calculo do preço total SEM desconto
 totalSemDesconto = valorProduto * quantidadeProduto
 print('O valor total SEM desconto é de: R$ {:.2f}'.format(totalSemDesconto))
 
-# calculo preço total COM desconto
 totalComDesconto = totalSemDesconto - totalSemDesconto * descontoProduto
 print('O valor total COM desconto é de R$ {:.2f}'.format(totalComDesconto))
