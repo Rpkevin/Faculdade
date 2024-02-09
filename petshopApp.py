a2cfd1f14943962d27f20493840ecf0765b50d97
Kevin Garcia <kevingarciarp@gmail.com>
2024-02-08 21:33:00 -0300

    sistema de cobrança de banho para um petshop.

--- petshopApp.py
+++ petshopApp.py
@@ -1,10 +1,8 @@
-# Função para calcular o valor base com base no peso do cachorro
 def cachorro_peso():
     while True:
         try:
-            #pede ao usuário para inserir o peso do cachorro
             peso = float(input("Digite o peso do cachorro em kg: "))
-            #Determina o valor c base no peso informado
+
             if peso < 3:
                 return 40
             elif peso < 10:
@@ -14,15 +12,14 @@ def cachorro_peso():
             elif peso < 50:
                 return 70
             else:
-                print("Cachorros acima de 50 kg não é suportado.")#msg se o peso for acima de 50kg
+                print("Cachorros acima de 50 kg não é suportado.")
         except ValueError:
-            print("Por favor, digite um valor numérico.") #qnd um valor inválido for inserido
+            print("Por favor, digite um valor numérico.") 
 
 
-# função p/ calcular o multiplicador c base no tipo de pelo do cachorro.
 def cachorro_pelo():
     while True:
-        #!pergunta sobre o tipo de pelo do cachorro.
+
         tipo_pelo = input("Digite o tipo de pelo do cachorro Pelo curto (C), Pelo Médio(M), Pelo Longo (L) (c/m/l): ")
         if tipo_pelo == 'c':
             return 1
@@ -31,35 +28,32 @@ def cachorro_pelo():
         elif tipo_pelo == 'l':
             return 2
         else:
-            #msg caso escreva uma opção inválida.
             print("Opção inválida. Escolha entre 'c', 'm' ou 'l'.")
 
 
-# !!!unção para calcular o valor extra com base nos serviços adicionais escolhidos!
 def cachorro_extra():
     extras = {'1': 10, '2': 12, '3': 15, '0': 0}
     total_extra = 0
 
     while True:
-        try: #pergunta se deseja um adicional.
+        try:
             print('Corte de unhas: 10R$(1) | Escovar os dentes: R$12 (2) | Limpeza de orelhas R$15,00 (3) | Não desejo mais mais nada. (0)')
             adicional = input("Deseja Algum serviço adicional? Digite (1/2/3/0): ")
 
             if adicional in extras:
-                total_extra += extras[adicional] #aq adc o valor adicional caso escolhido
+                total_extra += extras[adicional] 
                 if adicional == '0':
                     return total_extra
             else:
-                print("Opção inválida. Escolha entre '1', '2', '3' ou '0'.") #+1 msg caso uma msg escrita for inválida.
+                print("Opção inválida. Escolha entre '1', '2', '3' ou '0'.") 
         except ValueError:
             print("Por favor, digite um valor numérico.")
 
 
-# Função principal!
 def main():
     try:
-        print("Bem-vindo ao PetShop do Kevin G. Porto!") #cabeçalho
-        #1 pergunta. Nome do usuario.
+        print("Bem-vindo ao PetShop do Kevin G. Porto!") 
+
         nome = input("Digite seu nome: ")
         print(f"Olá, {nome}!")
 
@@ -67,12 +61,10 @@ def main():
         multiplicador = cachorro_pelo()
         total_extra = cachorro_extra()
 
-        #!calculo do valor total q é exibido no final.
         total = base * multiplicador + total_extra
         print(f"\nTotal a pagar: R${total:.2f}")
 
-    except ValueError: #pegar exceções caso entradas sejam inválidas.
+    except ValueError: 
         print("Ops, algo deu errado. Certifique-se de digitar números válidos.")
 
-# chamada da função principal sem uma frunção main especifica.
 main()
